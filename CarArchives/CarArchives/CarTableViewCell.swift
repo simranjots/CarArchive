9a1d73794d568dc3b31961214150b3ee60519b8f
simranjot singh <32786222+simranjots@users.noreply.github.com>
2020-03-24 19:36:28 -0700

    Add files via upload

---
+++ CarArchives/CarArchives/CarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarTableViewCell.swift
+//  CarArchives
+//
+//  Created by Simmun singh on 2020-03-24.
+//  Copyright © 2020 Simranjot. All rights reserved.
+//
+
+import UIKit
+
+class CarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CarArchives/CarArchives/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarArchives
+//
+//  Created by Simmun singh on 2020-03-24.
+//  Copyright © 2020 Simranjot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
